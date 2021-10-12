b0dd4f834bcc02d621e2c39bf8f3bd46ef080bed
Douglas Falcao <falcao.douglas97@gmail.com>
2021-10-12 05:00:40 -0300

    1.0

--- src/components/PasswordGenerator/index.js
+++ src/components/PasswordGenerator/index.js
@@ -1,11 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles.css';
 
 const PasswordGenerator = () => {
+    const [password, setPassword] = useState("test")
     const [passwordLength, setPasswordLength] = useState(30)
-    const [digitLength, setDigitLength] = useState(64)
-    const [symbolsLength, setSymbolsLength] = useState(20)
+    const [digitLength, setDigitLength] = useState(2)
+    const [symbolsLength, setSymbolsLength] = useState(2)
 
+    useEffect(() => {
+        const draftPassword = [];
+
+        draftPassword.push(randomDigit())
+        draftPassword.push(randomDigit())
+        draftPassword.push(randomSymbol())
+        draftPassword.push(randomSymbol())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+        draftPassword.push(randomLetter())
+
+        setPassword(draftPassword.slice(0, passwordLength).sort(() => Math.random() - 0.5).join(""))
+
+    },[passwordLength, digitLength, symbolsLength]);
+
+    const randomDigit = () => {
+        const digits = "0123456789"
+
+        return digits[Math.floor(Math.random() * digits.length)];
+    }
+
+    const randomSymbol = () => {
+        const symbols = "!@#$%&*({}^~.,/+[]"
+
+        return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+
+    const randomLetter = () => {
+        const letters = "abcdefghijklmnopqrstuvwxyz"
+
+        const letter = letters[Math.floor(Math.random() * letters.length)];
+
+
+        return Math.random() >= 0.5 ? letter : letter.toUpperCase();
+    }
 
     return <>
         <div className="slider">
@@ -15,16 +56,18 @@ const PasswordGenerator = () => {
         </div>
 
         <div className="slider">
-            <label htmlFor="digit">Tamanho  </label>
-            <input id="digit-length" type="range" min={4} max={64} value={digitLength} onChange={({target}) => setDigitLength(parseInt(target.value))}/>
+            <label htmlFor="digit">Dígitos  </label>
+            <input id="digit-length" type="range" min={0} max={10} value={digitLength} onChange={({target}) => setDigitLength(parseInt(target.value))}/>
             <span>{digitLength}</span>
         </div>
 
         <div className="slider">
-            <label htmlFor="symbols-Length">Tamanho  </label>
-            <input id="symbols-Length" type="range" min={4} max={64} value={symbolsLength} onChange={({target}) => setSymbolsLength(parseInt(target.value))}/>
+            <label htmlFor="symbols-Length">Símbolos  </label>
+            <input id="symbols-Length" type="range" min={0} max={10} value={symbolsLength} onChange={({target}) => setSymbolsLength(parseInt(target.value))}/>
             <span>{symbolsLength}</span>
         </div>
+
+        <h1 style={{ marginTop:"2rem", fontFamily: "monospace", textAlign: "center"}}>{password}</h1>
 
     </>
 };
